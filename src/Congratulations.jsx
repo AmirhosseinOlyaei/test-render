4848c39ba186f21a90d3116d0cc9aba0ab909d55
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-02-28 14:38:32 -0500

    Your First Component & Importing and Exporting Components

---
+++ src/Congratulations.jsx
@@ -0,0 +1,5 @@
+// Write your component below!
+function Congratulations() {
+  return <h1>Good Job!</h1>;
+}
+export default Congratulations;
---
+++ src/Profile.jsx
@@ -0,0 +1,7 @@
+function Profile() {
+  return (
+    <img src="https://via.placeholder.com/300x200" alt="Image placeholder" />
+  );
+}
+
+export default Profile;
