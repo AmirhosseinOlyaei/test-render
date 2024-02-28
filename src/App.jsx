8983bf0f53f28ca65bfbd45841b3115f95f49760
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-02-28 14:57:46 -0500

    Writing Markup with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Bio from "./Bio";
 import Congratulations from "./Congratulations";
 import Gallery from "./Gallery";
 
 function App() {
   return (
     <>
+      <Bio />
       <Gallery />
       <Congratulations />
     </>
